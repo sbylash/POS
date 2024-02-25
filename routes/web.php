50ae9df762a03eaef21beb3071e0ef7bf929f603
Shasia Sasa Salsabyla <salsabyla3008@gmail.com>
2024-02-25 12:09:41 +0700

    update [praktikum jobsheet 2]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Home
+Route::get('/', [HomeController::class, 'home']);
+
+//Product
+Route::prefix('products')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
+
+//User
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+
+//Sales
+Route::get('/sales', [SalesController::class, 'sales']);
